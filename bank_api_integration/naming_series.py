c4dcee93ebf8ae55e93b74ea5303feb47cc0f1b0
NaviN <navin@aerele.in>
2023-04-13 15:46:52 +0530

    updated naming_series

--- bank_api_integration/naming_series.py
+++ bank_api_integration/naming_series.py
@@ -4,6 +4,6 @@ import time
 def name_outward_bank_payment(doc, action):
 	ym = time.strftime("%y%m")
 	if "gta.lnder.in" in frappe.utils.get_url():
-		doc.name = parse_naming_series(f'OBP{ym}-.#####')
+		doc.name = parse_naming_series(f'OBP{ym}.#####')
 	else:
-		doc.name = parse_naming_series(f'DDBP{ym}-.#####')
+		doc.name = parse_naming_series(f'DDBP{ym}.#####')
